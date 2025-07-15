9ade34db79e4d4b3fde30af3f304bd6284b4fb48
luis reyes <luissre61@gmail.com>
2025-07-14 22:13:19 -0400

    fix: Update site URL in Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,16 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
+// Importar el plugin de vite para tailwind
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  // URL base correcta con tu nombre de usuario de GitHub
+  site: 'https://luissre61.github.io',
+  // El nombre de tu repositorio
+  base: '/luisreyesdigital',
   integrations: [mdx(), sitemap()],
-
   vite: {
     plugins: [tailwindcss()],
   },
